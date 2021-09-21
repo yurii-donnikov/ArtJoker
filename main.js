f53d06320301ac3217728034fc7da6645c524866
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-09-21 21:48:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-let matrix = [
+let haveMatrix = [
     [9, 3, 0],
     [9, 0, 5],
     [8, 1, 5],
     [7, 0, 5],
 ]
-for (let i = 0; i < matrix.length; i++) {
-    for (let index = 0; index < matrix[i].length; index++) {
-        if (matrix[i][index] == 0) {
-            matrix.splice(i, 1);
+for (let i = 0; i < haveMatrix.length; i++) {
+    for (let j = 0; j < haveMatrix[i].length; j++) {
+        if (haveMatrix[i][j] == 0) {
+            haveMatrix.splice(i, 1);
         };
     };
 };
-console.log(matrix)
+console.log(haveMatrix)
