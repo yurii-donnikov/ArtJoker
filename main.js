5978bdf19770251f09b9dc9a19a2dc584d4e5a5c
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-09-21 21:16:17 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,19 @@ function getColor() {
     return arguments[index]
 }
 getColor('red', 'yellow', 'green')
+
+let matrix = [
+    [9, 3, 0],
+    [9, 0, 5],
+    [8, 1, 5],
+    [7, 0, 5],
+]
+
+for (i = 0; i < matrix.length; i++) {
+    for (index = 0; index < matrix.length; index++) {
+        if (matrix[i][index] == 0) {
+            matrix.splice(i, 1)
+        }
+    }
+}
+console.log(matrix)
