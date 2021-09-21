79f2ba6121a5110555bed883ea2001ebe73167d4
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-09-21 21:16:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-let index = -1
 
-function getColor() {
-    index++
-    if (index > arguments.length - 1) {
-        index = 0
-    }
-    return arguments[index]
-}
-getColor('red', 'yellow', 'green')
 
 let matrix = [
     [9, 3, 0],
