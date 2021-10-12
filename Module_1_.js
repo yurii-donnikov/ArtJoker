a0d19221573b7bab82bd49df52f3af6f097b6ac3
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-10-12 17:30:43 +0300

    Update Module_1_.js

--- Module_1_.js
+++ Module_1_.js
@@ -665,6 +665,34 @@ function getRecursFactorial (number, count) {
        return 1;
    };
 };
+//18.1
+function MonogrammaRecursion (wordOne,wordTwo, count1, count2, arrayWordOne, arrayWordTwo) {
+    count1 = count1 || 0;
+    count2 = count2 || 0; 
+    arrayWordOne = arrayWordOne || ('' + wordOne).split('');
+    arrayWordTwo = arrayWordTwo || ('' + wordTwo).split('');
+    if (arrayWordOne.length != arrayWordTwo.length) {
+        return false;
+    };
+        if (arrayWordOne.toString() == arrayWordTwo.toString()){
+            return true;
+        } else {
+            if(arrayWordOne[count1] == arrayWordTwo[count2] || count2 > arrayWordOne.length - 1) {
+                if (count1 > arrayWordOne.length - 1){
+                    return false;
+                };
+                let deleteElem = arrayWordTwo.splice(count2, 1);
+                arrayWordTwo.push(deleteElem[0]);
+                count2 = 0;
+                return MonogrammaRecursion(wordOne,wordTwo, ++count1, count2, arrayWordOne, arrayWordTwo);
+            } else {
+                if (count1 > arrayWordOne.length - 1 || count2 > arrayWordOne.length - 1){
+                    return false;
+                };
+                return MonogrammaRecursion(wordOne,wordTwo, count1, ++count2, arrayWordOne, arrayWordTwo);
+            };
+        };
+};
 
 
 
