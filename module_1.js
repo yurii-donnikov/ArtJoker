ead46f8bd1fcd20d920bcb677217ab7a16871ab8
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-10-20 17:56:48 +0300

    Update module_1.js

--- module_1.js
+++ module_1.js
@@ -1,5 +1,5 @@
 //les1
-function Monogramma(wordOne, wordTwo) {
+function monogramma(wordOne, wordTwo) {
     let arrayWordOne = wordOne.split('');
     let arrayWordTwo = wordTwo.split('');
     if (arrayWordOne.length == arrayWordTwo.length) {
