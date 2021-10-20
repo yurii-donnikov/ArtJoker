e4bcbb318e7c4bd985194f93bb20ba4d95113a48
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-10-20 13:39:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ Function.prototype.myCall = function (obj, ...arg) {
     copyObj.func = this;
     return copyObj.func(...arg);
 }
-Function.prototype.myBind = function (obj) {
+Function.prototype.myBind = function (obj, ...arg) {
     let copyObj = Object.assign({}, obj);
     copyObj.func = this;
-    return function result () {
+    return function result (...arg) {
         copyObj.func();
     }
 }
