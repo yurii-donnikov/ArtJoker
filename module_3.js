d0dd0a0efe1215374bcb9d9df7c0a8299759b706
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-10-25 00:30:38 +0300

    Update module_3.js

--- module_3.js
+++ module_3.js
@@ -48,43 +48,77 @@ class Node {
         }
     }
 
-    remove(value, node, parent){
+
+
+    remove(value, node, linkParent, flag){
         node = node || this;
-        parent = parent || null;
-        if(node.#value === value) {
-            console.log('rgd')
-            console.log(parent.#value)
-            console.log('rg')
-            //console.log(this)
+        linkParent = linkParent || this;
+        flag = flag || false;
+
+
+
+        if(flag) {
+          if (node.#right !== null) {
+          return this.remove(value, node.#right, node, flag);
+          } else {
+            linkParent.#right = null;
+            return node.#value;
+          }
         }
-            if(node.#value < value) {
-                //console.log(node)
-                return this.remove(value, node.#right, node)
+
+
+        if(node.#value === value) {
+          if (node.#left === null && node.#right === null){
+            console.log(linkParent)
+            console.log('rgr')
+              if(linkParent.#left.#value === node.#value){
+                linkParent.#left = null;
+                return true;
+              } else{
+                linkParent.#right = null;
+                return true;
+              }
+          }
+          if(node.#left === null && node.#right){
+              node.#value = node.#right.#value;
+              node.#right = node.#right.#right;
+              return true;
+          }
+          if(node.#left && node.#right === null){
+              node.#value = node.#left.#value;
+              node.#left = node.#left.#left;
+              return true;
+          }
+          if(node.#left && node.#right) {
+            if(node.#left.#right) {
+              flag = true;
+              node.#value = this.remove(value, node.#left, node, flag)
+              flag = false;
+              return true;
+            } else {
+              node.#value = node.#left.#value;
+              node.#left = node.#left.#left;
+              return true;
             }
-
-            // if (node.#left === null && node.#right === null){
-            //     node.#value = null;
-            // }
-            // if(node.#left === null && node.#right){
-            //     node.#value = node.#right.#value
-            //     node.#left = node.#right.#left
-            //     node.#right = node.#right.#right
-            //     console.log(node)
-            // }
-            // if (node.#right === null && node.#left) {
-
-            // }
-      
+          }
+        } else {
+          if(node.#value < value) {
+              return this.remove(value, node.#right, node, flag)
+          }
+          if(node.#value > value) {
+              return this.remove(value, node.#left, node, flag)
+          }
+        }
     }
 }
 
 
 let node = new Node();
-node.insert(10);
-node.insert(20);
-node.insert(30);
-node.insert(40);
 node.insert(50);
+node.insert(60);
+node.insert(70);
+node.insert(75);
+node.insert(65);
+node.insert(66);
+
 console.dir(node)
-
-
