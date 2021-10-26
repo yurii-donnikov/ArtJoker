1f4f7c1c6cd556c715d0c73da67930455953a638
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-10-26 19:10:57 +0300

    Update chernovic.js

--- chernovic.js
+++ chernovic.js
@@ -12,3 +12,56 @@ Function.prototype.myBind = function (object, ...arg) {
     }
 }
 let newFunc2 = func.myBind( obj, 3000, 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let obj = {
+    a: 1,
+}
+
+
+let func = function(c, d) {return this.a + c + d};
+let bb = func.bind(obj, 40, 30)
+
+
+let a = 100
+Function.prototype.a = 6666
+
+Function.prototype.myBind = function(object, ...arg) {
+    let objCopy = Object.assign(object);
+
+    objCopy.func = this
+
+    //objCopy.func.prototype = objCopy
+    console.log(objCopy.func.prototype.a)
+
+    return objCopy
+}
+
+
+
+
+Function.prototype.myBind = function (object, ...arg) {
+    let copyObject = Object.assign({}, object);
+    copyObject.func = this;
+    return function () {
+        return copyObject.func(...arg)
+    }
+}
+let bnd = func.myBind(obj)
