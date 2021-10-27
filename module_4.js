66c1b4c7355c40c103f84e5d55182c5feba9d3a4
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-10-27 17:11:19 +0300

    Update module_4.js

--- module_4.js
+++ module_4.js
@@ -11,7 +11,7 @@ class Restaurant {
             },
             {
                 number: 4,
-                name: 'cook2'
+                name: 'dessert cook'
             },
 
         ];
@@ -25,63 +25,63 @@ class Restaurant {
                 department: 2,
             },
             {
-                name:'ivan2',
+                name:'mari',
                 position: 'assistant-cook',
                 salary: 700,
                 works: true,
                 department: 2,
             },
             {
-                name:'ivan3',
+                name:'anna',
                 position: 'assistant-cook',
                 salary: 600,
                 works: true,
                 department: 2,
             },
             {
-                name:'ivan3',
+                name:'dima',
                 position: 'cleaner',
                 salary: 500,
                 works: true,
                 department: 2,
             },
             {
-                name:'ivan4',
-                position: 'assistant-cook',
+                name:'igor',
+                position: 'dessert cook',
                 salary: 500,
                 works: false,
                 department: 4,
             },
             {
-                name:'ivan5',
+                name:'maks',
                 position: 'barman',
                 salary: 900,
                 works: true,
                 department: 1,
             },
             {
-                name:'ivan6',
+                name:'oleg',
                 position: 'ofitsiant',
                 salary: 800,
                 works: true,
                 department: 1,
             },
             {
-                name:'ivan7',
+                name:'marina',
                 position: 'ofitsiant',
                 salary: 700,
                 works: true,
                 department: 1,
             },
             {
-                name:'ivan17',
+                name:'rita',
                 position: 'ofitsiant',
-                salary: 7000,
+                salary: 1000,
                 works: true,
                 department: 1,
             },
             {
-                name:'ivan8',
+                name:'semen',
                 position: 'ofitsiant',
                 salary: 600,
                 works: false,
@@ -89,155 +89,88 @@ class Restaurant {
             }
         ]
     }
-    sumSalary(callback){
-        let result = {}
-        this.employee.forEach((i) => {
-          if(callback(i)) {
-        // колбек nn.sumSalary((i) => i.works !== true)
+    sumSalary(){
+        let result = {};
+        if(this.department && this.employee) {
+            this.employee.forEach((i) => {
                 if(result[i.department]) {
                     result[i.department] += i.salary;
                 } else {
                     result[i.department] = i.salary;
                 }
-            }
-        })
-        return result;
+            })
+            return result;
+        }
+        return null;
     }
-
-
-
-    // averageSalary(){
-
-    //   let result = {};
-    //   let result2 = {};
-    //   if(this.department && this.employee) {
-    //   this.department.forEach((i) => {
-    //     this.employee.forEach((j) => {
-    //         if(i.number === j.department) {
-    //             if(result[i.number]){
-    //                 result[i.number].push(j.salary)
-    //             } else {
-    //                 result[i.number] = [];
-    //                 result[i.number].push(j.salary)
-    //             }
-    //         }
-    //     })
-    //   });
-    // }
-    //   for (let i in result) {
-    //     let length = result[i].length;
-    //         for(let j = 0; j < result[i].length; j++) {
-                
-    //             if(result2[i]){
-    //                 result2[i] += result[i][j]
-    //             }else{
-    //                 result2[i] = result[i][j]
-    //             }
-    //             if(j === result[i].length - 1){
-    //                 result2[i] /= length
-    //             }
-    //         }
-    //   }
-    //   return result2
-    // }
-
     averageSalary(){
-        let sortEmploee = {};
+        let allSalary = {};
         let result = {};
         if(this.department && this.employee) {
         this.department.forEach((i) => {
           this.employee.forEach((j) => {
               if(i.number === j.department) {
-                  if(sortEmploee[i.number]){
-                    sortEmploee[i.number].push(j.salary)
+                  if(allSalary[i.number]){
+                    allSalary[i.number].push(j.salary);
                   } else {
-                    sortEmploee[i.number] = [];
-                    sortEmploee[i.number].push(j.salary)
+                    allSalary[i.number] = [];
+                    allSalary[i.number].push(j.salary);
                   }
               }
           })
-        });
+        })
       } else {
           return null;
       }
-        for (let i in sortEmploee) {
-          let length = sortEmploee[i].length;
-              for(let j = 0; j < sortEmploee[i].length; j++) {
-                  
+        for (let i in allSalary) {
+          let length = allSalary[i].length;
+              for(let j = 0; j < allSalary[i].length; j++) {
                   if(result[i]){
-                      result[i] += sortEmploee[i][j]
+                      result[i] += allSalary[i][j];
                   }else{
-                      result[i] = sortEmploee[i][j]
+                      result[i] = allSalary[i][j];
                   }
-                  if(j === sortEmploee[i].length - 1){
-                      result[i] /= length
+                  if(j === allSalary[i].length - 1){
+                      result[i] /= length;
                   }
               }
         }
-        return result
-      }
-
-    //   salaryMaxAndMin(){
-    //       let sortEmploee = {}
-    //     if(this.department && this.employee) {
-    //         this.department.forEach((i) => {
-    //             sortEmploee[i.number] = {} 
-    //             this.employee.forEach((j) => {
-    //                 if(i.number === j.department){
-    //                     if(sortEmploee[i.number][j.position]){
-    //                         sortEmploee[i.number][j.position].push(j.salary)
-    //                     } else {
-    //                         sortEmploee[i.number][j.position] = [];
-    //                         sortEmploee[i.number][j.position].push(j.salary)
-    //                     }
-                   
-    //                 }
-    //             })
-    //         })
-    //     }
-    //    // return sortEmploee
-
-    //   }
-
-    salaryMaxAndMin2(){
-        let sortEmploee = {}
-      if(this.department && this.employee) {
+        return result;
+    }
+    salaryMaxAndMin(){
+        let result = {};
+        if(this.department && this.employee) {
           this.department.forEach((i) => {
-              sortEmploee[i.number] = {} 
+            result[i.number] = {};
               this.employee.forEach((j) => {
                   if(i.number === j.department){
-                      if(sortEmploee[i.number][j.position]){
-                            if(sortEmploee[i.number][j.position].min > j.salary){
-                                sortEmploee[i.number][j.position].min = j.salary
+                      if(result[i.number][j.position]){
+                            if(result[i.number][j.position].min > j.salary){
+                                result[i.number][j.position].min = j.salary;
                             }
-                            if(sortEmploee[i.number][j.position].max < j.salary){
-                                sortEmploee[i.number][j.position].max = j.salary
+                            if(result[i.number][j.position].max < j.salary){
+                                result[i.number][j.position].max = j.salary;
                             }
-                                
-                          //sortEmploee[i.number][j.position].push(j.salary)
                       } else {
-                          sortEmploee[i.number][j.position] = {};
-                          sortEmploee[i.number][j.position].min = j.salary
-                          sortEmploee[i.number][j.position].max = j.salary
-
-                          //sortEmploee[i.number][j.position].push(j.salary)
+                        result[i.number][j.position] = {};
+                          result[i.number][j.position].min = j.salary;
+                          result[i.number][j.position].max = j.salary;
                       }
-                 
                   }
               })
           })
+          return result;
       }
-     return sortEmploee
-      
+      return null;
     }
     amountDismissed () {
         let count = 0;
         this.employee.forEach((i) => {
             if(i.works === false) {
-                count++
+                count++;
             }
         })
-        return count
+        return count;
     }
     departmentWithoutLeader(){
         let withLeader = {};
@@ -249,13 +182,10 @@ class Restaurant {
         })
         this.department.forEach((i) => {
            if(!withLeader[i.number]){
-               result.push(i.number)
+               result.push(i.number);
            }
         })
         return result;
     }
 }
-
-
-
 let nn = new Restaurant()
