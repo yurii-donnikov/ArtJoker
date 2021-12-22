41b9a5360b252d3f5ba0278a596ccd5551378740
Yurii Donnikov <66084797+yurii-donnikov@users.noreply.github.com>
2021-12-22 13:22:26 +0200

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,13 +1,3 @@
-let chessBoard2 = [
-  [0, 0, 0, 0, 0, 0, 0, 10],
-  [0, 0, 0, 0, 0, 0, 0, 11],
-  [0, 0, 0, 0, 0, 0, 0, 2],
-  [0, 0, 0, 0, 0, 0, 0, 3],
-  [0, 0, 0, 0, 0, 0, 0, 4],
-  [0, 0, 0, 0, 0, 0, 0, 5],
-  [0, 0, 0, 0, 0, 0, 0, 6],
-  [0, 0, 0, 0, 0, 0, 0, 7],
-];
 let chessBoard = [
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
@@ -19,48 +9,56 @@ let chessBoard = [
   [0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-//function createFerz(array, indexArray, indexNumber, isFlag) {
-//  //let zeroArray = [0, 0, 0, 0, 0, 0, 0, 0];
-//  indexArray = indexArray | 0;
-//  indexNumber = indexNumber | 0;
-//  isFlag = isFlag | false;
-//  for (let i = indexArray; i < array.length; i++) {
-//    for (let j = indexNumber; j < array[i].length; j++) {
-//      if (isFlag) {
-//        if (array[i][j] === 1) {
-//          console.log(array[i]);
-//          console.log(i, j);
-//          console.log(array[i][j]);
-//          array[i][j] = 0;
-//          console.log(array[i]);
-//          isFlag = false;
-//          continue;
-//        }
-//      } else {
-//        if (
-//          watchVerticle(i, j, array) &&
-//          watchRightBottomDiagonal(i, j, array) &&
-//          watchLeftBottomDiagonal(i, j, array) &&
-//          watchRightTopDiagonal(i, j, array) &&
-//          watchLeftTopDiagonal(i, j, array)
-//        ) {
-//          array[i][j] = 1;
-//          console.log(array[i]);
-//          console.log(i, j);
-//          break;
-//        } else {
-//          if (j === array[i].length - 1) {
-//            isFlag = true;
-//            j = 0;
-//            i--;
-//            return createFerz(array, i, j, isFlag);
-//          }
-//        }
-//      }
-//    }
-//  }
-//  return array;
-//}
+function watchVerticle(indexArr, indexNum, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][indexNum] === 1) {
+      return false;
+    }
+    if (i === array.length - 1 && array[i][indexNum] !== 1) {
+      return true;
+    }
+  }
+}
+
+function watchRightBottomDiagonal(indexArr, indexNum, array) {
+  if (array[indexArr][indexNum] !== 1) {
+    if (array[++indexArr] && ++indexNum < array.length) {
+      return watchRightBottomDiagonal(indexArr, indexNum, array);
+    }
+    return true;
+  }
+  return false;
+}
+
+function watchLeftBottomDiagonal(indexArr, indexNum, array) {
+  if (array[indexArr][indexNum] !== 1) {
+    if (array[++indexArr] && --indexNum > -1) {
+      return watchLeftBottomDiagonal(indexArr, indexNum, array);
+    }
+    return true;
+  }
+  return false;
+}
+
+function watchRightTopDiagonal(indexArr, indexNum, array) {
+  if (array[indexArr][indexNum] !== 1) {
+    if (array[--indexArr] && ++indexNum < array.length) {
+      return watchRightTopDiagonal(indexArr, indexNum, array);
+    }
+    return true;
+  }
+  return false;
+}
+
+function watchLeftTopDiagonal(indexArr, indexNum, array) {
+  if (array[indexArr][indexNum] !== 1) {
+    if (array[--indexArr] && --indexNum > -1) {
+      return watchLeftTopDiagonal(indexArr, indexNum, array);
+    }
+    return true;
+  }
+  return false;
+}
 
 function createFerz(array, indexArray, indexNumber, isFlag) {
   indexArray = indexArray | 0;
@@ -69,7 +67,6 @@ function createFerz(array, indexArray, indexNumber, isFlag) {
   for (let i = indexArray; i < array.length; i++) {
     for (let j = indexNumber; j < array[i].length; j++) {
       if (isFlag) {
-        //console.log(array[i][j]);
         if (array[i][j] === 1) {
           if (j === array[i].length - 1) {
             array[i][j] = 0;
@@ -83,10 +80,7 @@ function createFerz(array, indexArray, indexNumber, isFlag) {
               continue;
             }
           }
-          //console.log(array[i]);
         }
-
-        //break;
       } else {
         if (
           watchVerticle(i, j, array) &&
@@ -111,56 +105,4 @@ function createFerz(array, indexArray, indexNumber, isFlag) {
   return array;
 }
 
-function watchVerticle(indexArr, indexNum, array) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i][indexNum] === 1) {
-      return false;
-    }
-    if (i === array.length - 1 && array[i][indexNum] !== 1) {
-      return true;
-    }
-  }
-}
-
-function watchRightBottomDiagonal(indexArr, indexNum, array) {
-  if (array[indexArr][indexNum] !== 1) {
-    if (array[++indexArr] && ++indexNum < 8) {
-      //console.log(array[indexArr], indexNum);
-      return watchRightBottomDiagonal(indexArr, indexNum, array);
-    }
-    return true;
-  }
-  return false;
-}
-function watchLeftBottomDiagonal(indexArr, indexNum, array) {
-  if (array[indexArr][indexNum] !== 1) {
-    if (array[++indexArr] && --indexNum > -1) {
-      //console.log(array[indexArr], indexNum);
-      return watchLeftBottomDiagonal(indexArr, indexNum, array);
-    }
-    return true;
-  }
-  return false;
-}
-
-function watchRightTopDiagonal(indexArr, indexNum, array) {
-  if (array[indexArr][indexNum] !== 1) {
-    if (array[--indexArr] && ++indexNum < 8) {
-      //console.log(array[indexArr], indexNum);
-      return watchRightTopDiagonal(indexArr, indexNum, array);
-    }
-    return true;
-  }
-  return false;
-}
-
-function watchLeftTopDiagonal(indexArr, indexNum, array) {
-  if (array[indexArr][indexNum] !== 1) {
-    if (array[--indexArr] && --indexNum > -1) {
-      //console.log(array[indexArr], indexNum);
-      return watchLeftTopDiagonal(indexArr, indexNum, array);
-    }
-    return true;
-  }
-  return false;
-}
+createFerz(chessBoard);
